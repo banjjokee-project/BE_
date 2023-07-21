238dae71fb71f328fa5f037603ad598cce0c8d28
Dongmin Kim <ddmkim94@gmail.com>
2023-07-21 15:15:50 +0900

    프로젝트 생성
    .gitignore 파일 추가, HELP.md 파일 삭제

---
+++ src/main/java/com/project/banjjokee/BanjjokeeApplication.java
@@ -0,0 +1,13 @@
+package com.project.banjjokee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanjjokeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanjjokeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/banjjokee/BanjjokeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.banjjokee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanjjokeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
